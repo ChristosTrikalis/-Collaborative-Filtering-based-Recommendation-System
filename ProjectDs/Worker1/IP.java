e6f049e9afdb2f503491827a3d63ef08ba3b8775
TrikalisChristos <43909927+ChristosTrikalis@users.noreply.github.com>
2019-11-02 12:51:25 +0200

    Update IP.java

--- ProjectDs/Worker1/IP.java
+++ ProjectDs/Worker1/IP.java
@@ -2,7 +2,7 @@ package Worker1;
 
 class IP
 {
-    static final String ipMaster = "192.168.1.6";
+    static final String ipMaster = "IP of Master/Server here";
     static final int portWorker1 = 5002;
     static final int portMaster = 5001;
 }
