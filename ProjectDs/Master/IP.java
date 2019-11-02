6f4b27a755a490d03ca47e05cb39e815c7f05a5c
TrikalisChristos <43909927+ChristosTrikalis@users.noreply.github.com>
2019-11-02 12:47:36 +0200

    Update IP.java

--- ProjectDs/Master/IP.java
+++ ProjectDs/Master/IP.java
@@ -5,12 +5,12 @@ public class IP
     static final int portMaster = 5001;
     static final int numberOfWorkers = 4;
     static final int portWorker1 = 5002;
-    static final String ipWorker1 = "192.168.9.20";
+    static final String ipWorker1 = "ip1";
     static final int portWorker2 = 5003;
-    static final String ipWorker2 = "192.168.9.20";
+    static final String ipWorker2 = "ip2";
     static final int portWorker3 = 5004;
-    static final String ipWorker3 = "192.168.9.19";
+    static final String ipWorker3 = "ip3";
     static final int portWorker4 = 5005;
-    static final String ipWorker4 = "192.168.9.19";
+    static final String ipWorker4 = "ip4";
 }
 
